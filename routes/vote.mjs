669933a3e8cdb9f454b17aa1453ec269da1c3ff9
Pharnuwat <se56160411@gmail.com>
2025-09-06 14:18:06 +0700

    fix: error 404

--- routes/vote.mjs
+++ routes/vote.mjs
@@ -8,14 +8,20 @@ const voteRouter = Router();
     try{
       const questionId = req.params.questionId;
       const vote = req.body.vote
+
+      const question = await connectionPool.query(
+        `select * from questions where id = $1`,
+        [questionId]
+      );
+
+      if (question.rows.length === 0) {
+        return res.status(404).json({ message: "Question not found." });
+      }
   
       const updatedQuestion = await connectionPool.query(
         `insert into question_votes (question_id, vote) VALUES ($1, $2)`,
         [questionId, vote]);
 
-      if(updatedQuestion.rows.length === 0){
-        return res.status(404).json({message: "Question not found."});
-      }
       return res.status(200).json({
         message: "Vote on the question has been recorded successfully.",
       });
@@ -30,14 +36,19 @@ const voteRouter = Router();
     try{
       const answerId = req.params.answerId;
       const vote = req.body.vote
-  
+      
+      const answer = await connectionPool.query(
+        `select * from answers where id = $1`,
+        [answerId]
+      );
+      if(answer.rows.length === 0){
+        return res.status(404).json({message: "Answer not found."});
+      }
+
       const updatedAnswer = await connectionPool.query(
         `insert into answer_votes (answer_id, vote) VALUES ($1, $2)`,
         [answerId, vote]);
 
-      if(updatedAnswer.rows.length === 0){
-        return res.status(404).json({message: "Answer not found."});
-      }
       return res.status(200).json({
         message: "Vote on the answer has been recorded successfully.",
       });
